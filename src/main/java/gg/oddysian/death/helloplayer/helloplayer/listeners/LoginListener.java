74e38c118145be68504e7f014822ba02bcf868a8
id624373 <Klopfer007>
2021-08-26 14:36:07 +0200

    registered the event in the event bus

--- src/main/java/gg/oddysian/death/helloplayer/helloplayer/listeners/LoginListener.java
+++ src/main/java/gg/oddysian/death/helloplayer/helloplayer/listeners/LoginListener.java
@@ -1,17 +1,21 @@
 package gg.oddysian.death.helloplayer.helloplayer.listeners;
 
 import net.minecraft.util.text.TextComponentString;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.PlayerEvent;
 import gg.oddysian.death.helloplayer.helloplayer.util.Scheduler;
 
 public class LoginListener {
+    public LoginListener(){
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
     // On every player login
     @SubscribeEvent
     public static void onLogin(PlayerEvent.PlayerLoggedInEvent event){
-
-            Scheduler.runAsyncTimer(event.player.getServer(), 100,1);
-            System.out.println("Login Event triggered!");
-            event.player.sendMessage(new TextComponentString("Hello " + event.player.getName()));
+        Scheduler.runAsyncTimer(event.player.getServer(), 100,1);
+        System.out.println("Login Event triggered!");
+        event.player.sendMessage(new TextComponentString("Hello " + event.player.getName()));
     }
 }
